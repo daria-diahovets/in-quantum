b4a42cee9c21a652a2e8c3bc020d53bb36bab689
anotherGU <lisicaalexander@gmail.com>
2025-05-03 16:28:16 +0300

    feat: subscription adaptive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ import Footer from "@/components/Footer/Footer";
 export default function Home() {
   return (
     <>
-      <Header />
+      {/* <Header />
       <Promo />
       <About />
-      <Education />
+      <Education /> */}
       {/* <Team /> */}
-      {/* <Subscriptions /> */}
+       <Subscriptions /> 
       {/* <Footer /> */}
     </>
   );
