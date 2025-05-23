2ae64e31ba140f82d7f62ac63503f5023934508b
daria-diahovets <daria.diahovets@gmail.com>
2025-05-23 10:08:12 +0300

    fix: fixed sitemap date

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://inquantum.com.ua",
-      lastModified: "2025-23-05",
+      lastModified: "2025-05-23",
     },
   ];
 }
